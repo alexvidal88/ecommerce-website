b1ae4db4280367bf32ff67ffe03c85c1c90aee74
Alex Vidal <alexr.vidalh@gmail.com>
2022-02-28 19:55:45 +1100

    finished styling menu-items and homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
-    <div className="App">
-        Hello world
+    <div>
+        <HomePage/>
     </div>
   );
 }
